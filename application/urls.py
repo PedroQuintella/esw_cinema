1417f316ea1a1ba4a50e422e187fdd1ae6d13de7
Pedro Quintella <pedro.q2000@outlook.com>
2021-04-16 01:50:43 -0300

    Templates

--- application/urls.py
+++ application/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView, SobreView, GenerosView
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('sobre/', SobreView.as_view(), name='sobre'),
+    path('generos/', GenerosView.as_view(), name='generos'),
 ]
--- application/views.py
+++ application/views.py
@@ -7,3 +7,12 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+
+class SobreView(TemplateView):
+    template_name = 'about.html'
+
+
+class GenerosView(TemplateView):
+    template_name = 'genre.html'
+
